c47d112075c6033bdbe3e391e2e95a90a8519ee7
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-02-26 16:22:02 +0100

    improve html metatags

--- server/app.py
+++ server/app.py
@@ -8,28 +8,15 @@ from dash import Dash
 from flask_caching import Cache
 
 class CustomDash(Dash):
-    """Custom Dash app which overrides the default HTML content. """
-
-    def __init__(self, *args, description=None, **kwargs):
-        self.description = description
-
-        super().__init__(
-            *args,
-            external_stylesheets=['style.css'],
-            update_title='Loading...',
-            **kwargs)
+    """Custom Dash app which overrides the default HTML index template. """
 
     def interpolate_index(self, **kwargs):
-        kwargs['description'] = self.description
-
         return '''
         <!DOCTYPE html>
         <html>
             <head>
-                {metas}
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>{title}</title>
-                <meta name="description" content="{description}">
+                {metas}
                 <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
                 <link rel="alternate icon" href="/assets/favicon.ico" />
                 {css}
@@ -43,8 +30,26 @@ class CustomDash(Dash):
         </html>
         '''.format(**kwargs)
 
-app = CustomDash(__name__, suppress_callback_exceptions=True)
-app.title = 'Media-Laboratory'
+
+title = 'Media-Laboratory'
+description = '''
+Data visualisation tool to share your visual analysis on the Web, allowing a smooth transition from experiments to production.
+'''
+meta_tags = [
+    {'name': 'description', 'content': description},
+    {'name': 'viewport', 'content':'width=device-width, initial-scale=1.0'},
+    {'property': 'og:title', 'content': title},
+    {'property': 'og:description', 'content': description},
+    {'property': 'og:type', 'content': 'website'},
+    {'name': 'robots', 'content': 'noindex, follow'}
+]
+app = CustomDash(
+    __name__,
+    title=title,
+    meta_tags=meta_tags,
+    update_title='Loading...',
+    suppress_callback_exceptions=True
+)
 server = app.server
 
 # Use Flask-Caching library to save callback results in a shared memory
