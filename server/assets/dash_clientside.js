02693f0a1b6f685a102a29bbf412355281ec7164
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-02-26 16:16:38 +0100

    change how document title and description are set

---
+++ server/assets/dash_clientside.js
@@ -0,0 +1,15 @@
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    clientside: {
+        setMeta: function(title, desc) {
+            console.log('ff ', title, desc);
+            if (title) {
+                document.title = title;
+                document.querySelector('meta[property="og:title"]').setAttribute('content', title)
+            }
+            if (desc) {
+                document.querySelector('meta[name="description"]').setAttribute('content', desc);
+                document.querySelector('meta[property="og:description"]').setAttribute('content', desc)
+            }
+        }
+    }
+});
--- server/index.py
+++ server/index.py
@@ -5,7 +5,7 @@ import logging
 # Plotly's Dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, ClientsideFunction
 from flask import redirect
 
 # our module
@@ -22,17 +22,37 @@ db = Database()
 # Webpage main layout
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
+    html.Div(id='page-content'),
+    html.Div(id='title-placeholder', style={'display':'none'}, **{'data-value': None}),
+    html.Div(id='desc-placeholder', style={'display':'none'}, **{'data-value': None}),
+    html.Div(id='blank-output', style={'display':'none'})
 ])
 
-# On the index page just show a working message
-index_layout = html.Div('It works!')
+# Set document title and description from the `data-value` attributes
+# of the `title-placeholder` and `desc-placeholder` divs.
+# See assets/dash_clientside.js for the javascript-side of the code
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='setMeta'
+    ),
+    Output('blank-output', 'children'),
+    [Input('title-placeholder', 'data-value'), Input('desc-placeholder', 'data-value')]
+)
 
-@app.callback(Output('page-content', 'children'), Input('url', 'pathname'))
+@app.callback(
+    [Output('page-content', 'children'),
+    Output('title-placeholder', 'data-value'),
+    Output('desc-placeholder', 'data-value')],
+    Input('url', 'pathname')
+)
 @mycache
 def display_page(pathname):
     if pathname == '/':
-        return index_layout
+        # On the index page just show a working message
+        index_layout = html.Div('It works!')
+
+        return index_layout, None, None
 
     # url example: 'http://example.com/:uid'
     visualisation_uid = pathname[1:]
@@ -40,17 +60,14 @@ def display_page(pathname):
     try:
         record = db.load_visualisation(visualisation_uid)
         if record:
-            app.title = record['title']
-            app.description = record['description']
-
-            return record['layout']
+            return record['layout'], record['title'], record['description']
 
         else:
-            return '404'
+            return '404', None, None
 
     except DatabaseError as err:
         logger.warning(err)
-        return '404'
+        return '404', None, None
 
 # Run this on developing/test
 if __name__ == '__main__':
