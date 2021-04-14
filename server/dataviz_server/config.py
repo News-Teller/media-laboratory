2273c5bb5c112ea37ef5dcc601123a3bda9d6501
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-04-14 10:11:35 +0200

    fix configs for dash app server-side

--- server/dataviz_server/config.py
+++ server/dataviz_server/config.py
@@ -24,10 +24,18 @@ class TestConfig(Config):
 
 # dash-app server-side config to work with the dispatcher
 def build_dashapp_server_configs(app_name: str) -> dict:
-    return {
+    config = {
         'name': app_name,
-        'assets_folder': f'/assets/{app_name}',
+        #'assets_folder':,
         'serve_locally': False,
         'requests_pathname_prefix': f'/{app_name}/',
         'suppress_callback_exceptions': True
     }
+
+    if os.getenv('ROOT_ASSETS_FOLDER'):
+        config['assets_folder'] = os.path.join(os.getenv('ROOT_ASSETS_FOLDER'), app_name)
+
+    if os.getenv('DASH_ASSETS_EXTERNAL_PATH'):
+        config['assets_external_path'] = os.getenv('DASH_ASSETS_EXTERNAL_PATH')
+
+    return config
