72f5300fbb9fd29eab7fb177b9e3885836f81589
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-04-15 15:20:25 +0200

    improve package setup

--- dataviz/dataviz/__init__.py
+++ dataviz/dataviz/__init__.py
@@ -1,1 +1,2 @@
 from .dataviz import DataViz
+from .version import __version__
--- dataviz/setup.py
+++ dataviz/setup.py
@@ -9,7 +9,7 @@ setup(
     long_description_content_type="text/markdown",
     author='Marco Romanelli',
     author_email="marco.romanelli@epfl.ch",
-    license='MIT',
+    license='GPLv3',
     url='https://github.com/News-Teller/media-laboratory',
     project_urls={
         "Issue Tracker": "https://github.com/News-Teller/media-laboratory/issues",
@@ -17,5 +17,27 @@ setup(
     },
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
     packages=find_packages(exclude=["tests*"]),
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Dash',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Widget Sets'
+    ],
+    install_requires=[
+        'dash>=1.15.0',
+        'dill>=0.3.0',
+        'pymongo'
+    ],
 )
